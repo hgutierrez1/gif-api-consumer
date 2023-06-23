267a1b792ee5900319d4ec5ef915188aa389f006
hgutierrez1 <hgutierrezva123@gmail.com>
2023-06-23 18:33:09 -0500

    fix bug #1: All giant gifs

--- Components/GifContainer.jsx
+++ Components/GifContainer.jsx
@@ -6,7 +6,7 @@ function GifContainer({src,alt}) {
 
   return (
     <figure className='relative h-60 w-60'>
-        <Image fill sizes='80vw' className='p-4 bg-green-palette' src={`${src}`} alt={alt} />
+        <Image fill sizes='100%' className='p-4 bg-green-palette' src={`${src}`} alt={alt} />
     </figure>
   )
 }
